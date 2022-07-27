bdfa68cc8660ef62b51d1929f0389a8bdbdaa0fa
Erva Ulusoy <95041228+ervau@users.noreply.github.com>
2022-07-28 00:33:00 +0300

    Update enrichment_analysis.py

--- bin/enrichment_analysis.py
+++ bin/enrichment_analysis.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import numpy as np
 import os
-# import matplotlib.pyplot as plt 
 
 cwd = os.getcwd()
 input_file_path = "{}Domain2GO/input_data".format(cwd.split("Domain2GO")[0])
 output_file_path = "{}Domain2GO/output".format(cwd.split("Domain2GO")[0])
 
 
-# plt.rcParams['figure.dpi']= 150
-# plt.rc("savefig", dpi=150)
 
 top_ks = [100, 250, 750] +  list(range(1000,10000, 250)) + list(range(10000,76000, 1000))
 
@@ -69,67 +66,3 @@ def run_enrichment_analysis(mapping_w_scores, full_mode = "no"):
 
     return s_enrichment, initial_theta_enrichment, theta_enrichment, e_enrichment
 
-# def enrichment_analysis_plot(mapping_w_scores, full_mode = "no"):
-
-#     if full_mode == "no":
-#         em_output_file = "{}/em_only_theta_output.txt".format(input_file_path)
-#         em_output = pd.read_csv(em_output_file, delimiter = "\t")
-    
-#     else:
-#         em_output_file = "{}/em_full_output.txt".format(input_file_path)
-#         em_output = pd.read_csv(em_output_file, delimiter = "\t")
-
-#     s_enrichment, initial_theta_enrichment, theta_enrichment, e_enrichment = run_enrichment_analysis(mapping_w_scores, em_output, full_mode)
-    
-#     major_ticks_index=[]
-#     major_ticks_list=[100,1000,2500, 5000, 7500, 10000, 20000, 30000, 40000, 50000, 60000, 70000]
-#     for tick in major_ticks_list:
-#         major_ticks_index.append(top_ks.index(tick))
-
-#     plt.ioff()
-#     fig = plt.figure()
-#     ax = fig.add_subplot()
-
-#     x1 = np.array(range(len(top_ks)))
-#     y1 = np.array(s_enrichment)
-
-#     x2 = np.array(range(len(top_ks)))
-#     y2 = np.array(theta_enrichment)
-
-#     x3 = np.array(range(len(top_ks)))
-#     y3 = np.array(initial_theta_enrichment)
-
-#     x4 = np.array(range(len(top_ks)))
-#     y4 = np.array(e_enrichment)
-
-#     ax.set_xticks(major_ticks_index)
-#     ax.set_xticks(np.setdiff1d(np.array(range(len(top_ks))), major_ticks_index), minor = True)
-
-#     ax.set_xticklabels(major_ticks_list)
-
-#     ax.tick_params(axis = 'x', which = 'major', labelsize = 8, rotation = 45)
-#     ax.tick_params(axis = 'y', which = 'major', labelsize = 8)
-#     ax.tick_params(axis = 'x', which = 'minor', labelsize = 0)
-
-#     ax.xaxis.grid(True, which='major', alpha= 0.4)
-
-
-#     plt.plot(x1, y1, label="S", color="#369694", linewidth=2)
-#     plt.plot(x3, y3, label="Initial θ", color="#3552C8", linewidth=2)
-#     plt.plot(x2, y2, label="θ", color="#7A72BD", linewidth=2)
-#     plt.plot(x4, y4, label="E", color="#9DD8B4", linewidth=2)
-
-#     fig.suptitle("", fontsize=15)
-#     plt.ylabel("Observed/expected Interpro2GO pairs", fontsize= 10)
-#     plt.xlabel("Cumulative rank", fontsize= 10)
-
-#     fig.set_figwidth(8)
-#     fig.set_figheight(5)
-#     plt.legend(loc='upper right', fontsize= 10)
-
-#     output_file = "{}/enrichment_analysis.png".format(output_folder)
-#     plt.savefig(output_file)
-
-#     def run_enrichment_analysis(initial_mapping_w_scores, full_mode = "no"):
-#         enrichment_analysis(initial_mapping_w_scores, full_mode)
-#         enrichment_analysis_plot(initial_mapping_w_scores, full_mode)
